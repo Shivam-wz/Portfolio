6647d052e3e54d7529eaa3032cdff9b0add40a52
Shivam-wz <56469003+Shivam-wz@users.noreply.github.com>
2024-05-10 10:29:25 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Shivam-wz",
   resume:
-    "https://drive.google.com/file/d/1seQGvpnCgD02eBovQnKgIE-fX8Dv3AAs/view?usp=drive_link",
+    "https://drive.google.com/file/d/1_guteeado6arFgIm2ekn8C3olhiZwqld/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/shivam-singh-b4532b192/",
 }
 
